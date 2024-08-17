d4e102f81b0d69e27472fac4a3afe71fbd3ca2d7
shantal rhobi <shantal.rohbi@student.moringaschool.com>
2024-08-18 00:09:19 +0300

    complete lab

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,24 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
   // add useEffect code
+  useEffect(()=> {
+    if(timeRemaining > 0){
+      const timerId = setTimeout(()=>{
+        setTimeRemaining((prevTime) => prevTime - 1);
+
+      }, 1000);
+
+      //cleaning the timer when time remaining hits 0
+      return() => clearTimeout(timerId);
+    } else{
+      //if time runs out , reset timer and trigger onAnswered callback with false
+      setTimeRemaining(10);
+      onAnswered(false);
+    }
+  }, [timeRemaining]);
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
